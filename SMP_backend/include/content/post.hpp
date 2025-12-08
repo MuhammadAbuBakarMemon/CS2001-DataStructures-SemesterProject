4135e4e4530dad84851f8ca0462f68ba9ff894cf
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-12-08 20:12:45 +0500

    up1 post.hpp//////////////

--- SMP_backend/include/content/post.hpp
+++ SMP_backend/include/content/post.hpp
@@ -18,4 +18,31 @@ struct Post {
 
     static unsigned long long nextPostID;
 
-    
+    Post() : id(0), authorID(0), timestamp(0) {}
+
+    Post(unsigned long long uid, string uname, string text) 
+        : id(nextPostID++), authorID(uid), authorUsername(uname), content(text), timestamp(time(nullptr)) {}
+
+    json toJSON() const {
+        return json{
+            {"id", id},
+            {"authorID", authorID},
+            {"authorUsername", authorUsername},
+            {"content", content},
+            {"timestamp", timestamp}
+        };
+    }
+
+    static Post fromJSON(const json& j) {
+        Post p;
+        p.id = j.at("id").get<unsigned long long>();
+        p.authorID = j.at("authorID").get<unsigned long long>();
+        p.authorUsername = j.at("authorUsername").get<string>();
+        p.content = j.at("content").get<string>();
+        p.timestamp = j.at("timestamp").get<time_t>();
+        
+        if (p.id >= nextPostID) {
+            nextPostID = p.id + 1;
+        }
+        return p;
+stID = 1;
