e8aef7bec4a28010584d1b5594262dd26f9778ba
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-12-08 20:15:11 +0500

    Update recommendation.hpp

--- SMP_backend/include/content/recommendation.hpp
+++ SMP_backend/include/content/recommendation.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include "core/user.hpp"
 #include "ADT/linked_list.hpp"
@@ -52,3 +51,4 @@ public:
         cout << "--------------------------------\n";
     }
 };
+
